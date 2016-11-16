6b4b7c753181dce7a6e3e383115e4574aade1c49
Tony Ko <htko89@gmail.com>
2016-11-16 05:45:21 -0500

    Fix: Wrong path and function names

--- fccSpider/pipelines.py
+++ fccSpider/pipelines.py
@@ -6,6 +6,6 @@
 # See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
 
 
-class tutorialPipeline(object):
+class fccSpiderPipeline(object):
     def process_item(self, item, spider):
         return item
--- fccSpider/settings.py
+++ fccSpider/settings.py
@@ -9,10 +9,10 @@
 #     http://scrapy.readthedocs.org/en/latest/topics/downloader-middleware.html
 #     http://scrapy.readthedocs.org/en/latest/topics/spider-middleware.html
 
-BOT_NAME = 'tutorial'
+BOT_NAME = 'fccSpider'
 
-SPIDER_MODULES = ['tutorial.spiders']
-NEWSPIDER_MODULE = 'tutorial.spiders'
+SPIDER_MODULES = ['fccSpider.spiders']
+NEWSPIDER_MODULE = 'fccSpider.spiders'
 
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
