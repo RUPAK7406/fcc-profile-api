2ea4a7b40d20977f3e89a56e7e7e78e44425ee62
Tony Ko <htko89@gmail.com>
2016-11-20 18:52:16 -0500

    Update: Folder names and script location
    Removed script from appplication directory

--- fccSpider.sh
+++ fccSpider.sh
@@ -5,17 +5,18 @@
 # global variables
   echo
   echo -e "Authentication:"
-  sudo echo "Sudo Permission"
+  sudo echo "Sudo permission to delete compiled python files in project"
 
 # action
   echo
   echo -e "Action:"
-  if [ -f "./spiders/$1_spider.py" ]; then
+  if [ -f "./scrapy/spiders/$1_spider.py" ]; then
     echo -e "+ Spider: ($1) with username: ($2)"
     set -x # echo commands
     set -x
-    find . -name "*.pyc" -type f -delete
+    sudo find ./scrapy -name "*.pyc" -type f -delete
     if [ -n "$2" ]; then
+      cd scrapy
       scrapy crawl $1 -o $1.json -t json -a username=$2
     else
       echo -e "Please supply a username"
@@ -26,6 +27,6 @@
     echo "+ Usage   : ./fccSpider.sh [spiderName] [username]"
     echo "+ Example : ./fccSpider.sh fccMap myfccusername"
     echo "+ Available spiders are:"
-    echo "+ "$(ls ./spiders/*_spider.py)
+    echo "+ "$(ls ./scrapy/spiders/*_spider.py)
   fi
   echo
