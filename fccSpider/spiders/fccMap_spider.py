9cf5aba5c1de39528397eedd42b6880cdc5ed38d
Tony Ko <htko89@gmail.com>
2016-11-21 03:39:46 -0500

    Update: Use orderedlist
    instead of dictionaries

--- fccSpider/spiders/fccMap_spider.py
+++ fccSpider/spiders/fccMap_spider.py
@@ -9,17 +9,15 @@ class fccMap(scrapy.Spider):
 
 
     def parse(self, response):
-        result = {}
+        result = OrderedDict()
         for table in response.css("#accordion"):
             # only one table, so no need to find titles. Skip a subsection for dictionary.
             for certIdx, cert in enumerate(table.css("div.certBlock")): #list certs in table and loop
                 certName = table.css("h2 > a::text").extract()[certIdx] #find the title
-                result[certName] = {} #set up empty dictionary under title
-                # result[certName]["order"] = certIdx
+                result[certName] = OrderedDict() #set up empty dictionary under title
                 for chapIdx, chap in enumerate(cert.css("div.chapterBlock")): #list chapters in cert and loop
                     chapName = cert.css("h3 > a::text").extract()[chapIdx] #find the title
-                    result[certName][chapName] = {} #set up empty dictionary under title
-                    # result[certName][chapName]["order"] = chapIdx
+                    result[certName][chapName] = OrderedDict() #set up empty dictionary under title
                     for chalIdx, chal in enumerate(chap.css("p.challenge-title")): #list challenges in chapters and loop
                         chalName = str(chal.css("a span::text").extract_first()).lstrip() #find the title
                         chalName2 = str(chal.css("a::text").extract_first()).lstrip() #find the title
@@ -31,7 +29,6 @@ class fccMap(scrapy.Spider):
                             'link': chalLink,
                             'status': chalStatus,
                         }
-                        # result[certName][chapName][chalName]["order"] = chalIdx
         return result
 
 # str("%02d"%certIdx) + " " +
