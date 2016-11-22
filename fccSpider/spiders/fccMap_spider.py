be2494dd3d5078dd2a7238f0c34748853715890f
Tony Ko <htko89@gmail.com>
2016-11-21 19:16:56 -0500

    Add: Metadata fields, better name & status selector

--- fccSpider/spiders/fccMap_spider.py
+++ fccSpider/spiders/fccMap_spider.py
@@ -7,7 +7,7 @@ class fccMap(scrapy.Spider):
     name = "fccMap"
     def __init__(self, username=None, *args, **kwargs):
         super(fccMap, self).__init__(*args, **kwargs)
-        self.start_urls = ['https://www.freecodecamp.com/map']
+        self.start_urls = ['http://local.htko.ca/map.html']
 
 
     def parse(self, response):
@@ -17,22 +17,34 @@ class fccMap(scrapy.Spider):
             # only one table, so no need to find titles. Skip a subsection for dictionary.
             for certIdx, cert in enumerate(table.css("div.certBlock")): #list certs in table and loop
                 certName = table.css("h2 > a::text").extract()[certIdx] #find the title
-                result[certName] = '' #set up empty dictionary under title
                 result[certName] = OrderedDict() #set up empty dictionary under title
                 for chapIdx, chap in enumerate(cert.css("div.chapterBlock")): #list chapters in cert and loop
                     chapName = cert.css("h3 > a::text").extract()[chapIdx] #find the title
+                    chapTime = cert.css("h3 > .challengeBlockTime::text").extract()[chapIdx] #find the title
                     result[certName][chapName] = OrderedDict() #set up empty dictionary under title
+                    result[certName][chapName]["_time"] = chapTime[1:-1]
                     for chalIdx, chal in enumerate(chap.css("p.challenge-title")): #list challenges in chapters and loop
-                        chalName = str(chal.css("a span::text").extract_first()).lstrip() #find the title
-                        chalName2 = str(chal.css("a::text").extract_first()).lstrip() #find the title
+                        chalName = chal.css("::text").extract_first()
                         chalLink = chal.css("a::attr(href)").extract_first() #find the link
-                        chalStatus = str(chal.css("a span.sr-only::text").extract_first()).lstrip() #find the status
-                        if chalName == chalStatus:
-                            chalName = chalName2
-                        result[certName][chapName][chalName] = {
-                            'link': chalLink,
-                            'status': chalStatus,
-                        }
+                        chalStatus = chal.css("a span.sr-only::text").extract_first() #find the status
+                        chalExist = chal.css("em::text").extract_first() #find if it exists
+                        if chalExist:
+                            chalStatus = chalExist
+                        if chal.css('.disabled.ion-locked'):
+                            chalStatus = "Locked"
+                        if not chalLink:
+                            chalLink = ""
+                        chalName = str(chalName).lstrip() #find the title
+                        chalStatus = str(chalStatus).lstrip() #find the status
+                        chalLink = str(chalLink).lstrip()
+                        result[certName][chapName][chalName] = OrderedDict()
+                        result[certName][chapName][chalName]["_status"] = chalStatus
+                        result[certName][chapName][chalName]["_link"] = chalLink
+                        result[certName][chapName][chalName]["_dateC"] = ''
+                        result[certName][chapName][chalName]["_dateU"] = ''
+                        result[certName][chapName][chalName]["_desc"] = ''
+                        result[certName][chapName][chalName]["_req"] = ''
+                        result[certName][chapName][chalName]["_code"] = ''
         self.log('Loop End')
         path = os.getcwd() + "/output/fccMap.json"
         self.log('Output: '+path)
