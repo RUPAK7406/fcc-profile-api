a27eb6e5c14a2d4e0a930d20023e059bec74b081
Tony Ko <htko89@gmail.com>
2016-11-24 19:08:58 -0500

    Fix: Use debug spider with debug script

--- fccSpider_2.sh
+++ fccSpider_2.sh
@@ -17,7 +17,7 @@
     mkdir output # make output directory
     # rm -f ./output/*.json # removes all previous outputs for clean operation
     find . -name "*.pyc" -type f -delete # clear compiled / cache python files for clean operation
-    scrapy crawl challenge -a username=$1
+    scrapy crawl challenge2 -a username=$1
     find . -name "*.pyc" -type f -delete
     set +x # echo off
   else
