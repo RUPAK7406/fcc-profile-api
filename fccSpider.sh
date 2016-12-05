83422504815cb22a91d7b7fb6867ab403d79993d
Tony Ko <htko89@gmail.com>
2016-12-04 22:04:27 -0500

    Update: Script to differentiate export from scrape
    To prevent excessive scraping.

--- fccSpider.sh
+++ fccSpider.sh
@@ -10,21 +10,28 @@
 # action
   echo
   echo -e "Action:"
-  if [ -n "$1" ]; then
-    echo -e "+ User: ($1)."
+  if [ -n "${1}" ]; then
+    echo -e "+ User: (${1})."
     set -x # echo commands
     set -x
-    mkdir output # make output directory
-    rm -f ./output/*.json # removes all previous outputs for clean operation
     find . -name "*.pyc" -type f -delete # clear compiled / cache python files for clean operation
-    scrapy crawl map -a username=$1
-    scrapy crawl profile -a username=$1
-    scrapy crawl challenge -a username=$1
+    if [ "${2}" == "scrape" ]; then
+      mkdir common # make output directory
+      mkdir export # make output directory
+      rm -f ./common/*.json # removes all previous outputs for clean operation
+      rm -f ./export/* # removes previous export data
+      scrapy crawl map -a username=${1}
+      scrapy crawl desc -a username=${1}
+      scrapy crawl profile -a username=${1}
+    elif [ "${2}" == "export" ]; then
+      scrapy crawl export -a username=${1}
+    fi
     find . -name "*.pyc" -type f -delete
     set +x # echo off
   else
     echo "+ Application Comments:"
-    echo "+ Usage   : ./fccSpider.sh [username]"
-    echo "+ Example : ./fccSpider.sh myfccusername"
+    echo "+ Usage   : ./fccSpider.sh [username] [action]"
+    echo "+ Example : ./fccSpider.sh htko89 scrape"
+    echo "+ Actions : scrape, export"
   fi
   echo
