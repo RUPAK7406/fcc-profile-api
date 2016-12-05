44912e668856e003e78b34267075a46a521c25ef
Tony Ko <htko89@gmail.com>
2016-12-04 22:29:26 -0500

    Add: Selective run profiles

--- fccSpider.sh
+++ fccSpider.sh
@@ -7,6 +7,14 @@
   echo "User permissions required for execution:"
   echo "Ability to delete compiled python files in project."
 
+# Clean Function
+clrData () {
+  mkdir common # make output directory
+  mkdir export # make output directory
+  rm -f ./common/*.json # removes all previous outputs for clean operation
+  rm -rf ./export/* # removes previous export data
+}
+
 # action
   echo
   echo -e "Action:"
@@ -15,14 +23,21 @@
     set -x # echo commands
     set -x
     find . -name "*.pyc" -type f -delete # clear compiled / cache python files for clean operation
-    if [ "${2}" == "scrape" ]; then
-      mkdir common # make output directory
-      mkdir export # make output directory
-      rm -f ./common/*.json # removes all previous outputs for clean operation
-      rm -f ./export/* # removes previous export data
+    if [ "${2}" == "full" ]; then
+      clrData
       scrapy crawl map -a username=${1}
       scrapy crawl desc -a username=${1}
       scrapy crawl profile -a username=${1}
+      scrapy crawl export -a username=${1}
+    elif [ "${2}" == "scrape" ]; then
+      clrData
+      scrapy crawl map -a username=${1}
+      scrapy crawl desc -a username=${1}
+      scrapy crawl profile -a username=${1}
+    elif [ "${2}" == "empty" ]; then
+      clrData
+      scrapy crawl map -a username=${1}
+      scrapy crawl desc -a username=${1}
     elif [ "${2}" == "export" ]; then
       scrapy crawl export -a username=${1}
     fi
@@ -31,7 +46,11 @@
   else
     echo "+ Application Comments:"
     echo "+ Usage   : ./fccSpider.sh [username] [action]"
-    echo "+ Example : ./fccSpider.sh htko89 scrape"
-    echo "+ Actions : scrape, export"
+    echo "+ Example : ./fccSpider.sh htko89 full"
+    echo "+ Actions : See below."
+    echo "++ full : Downloads the FCC curriculum map, challenge descriptions, personal completion profile, export results"
+    echo "++ scrape : Downloads the FCC curriculum map, challenge descriptions, personal completion profile"
+    echo "++ empty  : Downloads the FCC curriculum map, challenge descriptions."
+    echo "++ export : Exports existing results to export folder. Scrape must have run at least once before or errors will occur"
   fi
   echo
