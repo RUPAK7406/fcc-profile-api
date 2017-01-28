be2e4f2acdd752cb79042781a1aa45fd0136d4fb
Tony Ko <htko89@gmail.com>
2017-01-28 03:00:50 -0500

    Update: Use deprecated createserver for HTTPS until suitable solution found

--- release/server.js
+++ release/server.js
@@ -5,13 +5,20 @@
 var scrapers = require("./scrapers.js");
 var helmet = require("helmet");
 var express = require("express");
+var http = require("http");
+var https = require("https");
+var fs = require("fs");
 var app = express();
 app.use(helmet());
 
+// https certs
+// var cert = {
+//   key: fs.readFileSync("/srv/certs/shared.key", "utf8"),
+//   cert: fs.readFileSync("/srv/certs/shared.crt", "utf8")
+// };
+
 // main logic
 var time = new Date();
-var port = 80;
-
 
 app.get("/fcc", function(req, res) {
   scrapers.mapScrape(req.query.user, res, mapFinish); // scrape map first to build a json structure
@@ -24,7 +31,7 @@ function mapFinish(err, user, data, map, res) {
     });
   } else {
     if (user) { // If username is given, then scrape profile
-      if(data && map){
+      if (data && map) {
         scrapers.profileScrape(user, data, map, res, profileFinish);
       }
     } else {
@@ -33,7 +40,7 @@ function mapFinish(err, user, data, map, res) {
   }
 }
 
-function profileFinish (err, user, data, map, res) {
+function profileFinish(err, user, data, map, res) {
   if (err) {
     res.status(500).jsonp({
       "Errors": err
@@ -43,6 +50,9 @@ function profileFinish (err, user, data, map, res) {
   }
 }
 
-app.listen(port, function() {
-  console.info("[FCC-Profile-API]", "Server initialized at port " + port);
+http.createServer(app).listen(80, function () {
+   console.info("[FCC-Profile-API]", "HTTP server initialized");
+});
+https.createServer(cert, app).listen(443, function () {
+   console.info("[FCC-Profile-API]", "HTTPS server initialized");
 });
